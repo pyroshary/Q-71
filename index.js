6e820f2b5b131846f7c0351e137ba6489f635a4b
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-03 22:56:29 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var age = 15;
+age = 20;
+console.log(age);
+var name = "alice";
+try {
+    name = "Bob";
+}
+catch (error) {
+    console.log("ERROR can reasingn a `const` already a declared variable");
+}
---
+++ index.ts
@@ -0,0 +1,9 @@
+let age = 15;
+age = 20;
+
+console.log(age);
+
+const name = "alice";
+try{
+    name = "Bob";
+} catch(error){console.log("ERROR can reasingn a `const` already a declared variable");}
